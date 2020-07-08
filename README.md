daf22bc1e75d8d48fd6cd13ac33a5c1fe7a963da
Max Zakharzhevskiy <max.za@digitalpersona.com>
2020-07-07 22:27:04 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# Overview
+
+netsh-rule will generate a file that allows to apply firewall rule to the specified application or all executables in the specified folder.
+
+## Usage
+
+```netsh-rule <filename | folder> [options]```
+
+option  | description
+--------|--
+name    | The rule name (name will be generated if missing name)
+program | Absolute path to \<filename \| folder>
+enable  | Enable rule: yes \| no (default: yes)
+dir     | The rule is inbound or outbound: in \| out \| both (default: both i.e. in and out)
+action  | The action for rule: allow \| block (default: block)
+profile | Apply rule for: public \| private \| domain (default: public, private, domain)
+
+# \<netsh advfirewall firewall add rule> quick reference
+
+## Usage:
+```netsh advfirewall firewall add rule```
+
+*    name = \<string\>
+*    dir = in \| out
+*    action = allow \| block \| bypass
+*    [ program = \<program path\> ]
+*    [ service = \<service short name\> \| any ]
+*    [ description = \<string\> ]
+*    [ enable = yes \| no (default = yes) ]
+*    [ profile = public \| private \| domain \| any[ ,... ] ]
+*    [ localip = any \| \<IPv4 address\> \| \<IPv6 address\> \| \<subnet\> \| \<range\> \| \<list\> ]
+*    [ remoteip = any \| localsubnet \| dns \| dhcp \| wins \| defaultgateway \| \<IPv4 address\> \| \<IPv6 address\> \| \<subnet\> \| \<range\> \| \<list\> ]
+*    [ localport = 0-65535 \| \<port range\>[ ,... ] \| RPC \| RPC-EPMap \| IPHTTPS \| any (default = any) ]
+*    [ remoteport = 0-65535 \| \<port range\>[ ,... ] \| any (default = any) ]
+*    [ protocol = 0-255 \| icmpv4 \| icmpv6 \| icmpv4:type,code \| icmpv6:type,code \| tcp \| udp \| any (default = any) ]
+*    [ interfacetype = wireless \| lan \| ras \| any ]
+*    [ rmtcomputergrp = \<SDDL string\> ]
+*    [ rmtusrgrp = \<SDDL string\> ]
+*    [ edge = yes \| deferapp \| deferuser \| no (default = no) ]
+*    [ security = authenticate \| authenc \| authdynenc \| authnoencap \| notrequired (default = notrequired) ]
+
+## Remarks:
+
+ - Add a new inbound or outbound rule to the firewall policy.
+ - Rule name should be unique and cannot be "all".
+ - If a remote computer or user group is specified, security must be
+   authenticate, authenc, authdynenc, or authnoencap.
+ - Setting security to authdynenc allows systems to dynamically
+   negotiate the use of encryption for traffic that matches
+   a given Windows Firewall rule. Encryption is negotiated based on
+   existing connection security rule properties. This option
+   enables the ability of a machine to accept the first TCP
+   or UDP packet of an inbound IPsec connection as long as
+   it is secured, but not encrypted, using IPsec.
+   Once the first packet is processed, the server will
+   re-negotiate the connection and upgrade it so that
+   all subsequent communications are fully encrypted.
+ - If action=bypass, the remote computer group must be specified when dir=in.
+ - If service=any, the rule applies only to services.
+ - ICMP type or code can be "any".
+ - Edge can only be specified for inbound rules.
+ - AuthEnc and authnoencap cannot be used together.
+ - Authdynenc is valid only when dir=in.
+ - When authnoencap is set, the security=authenticate option becomes an
+   optional parameter.
+
+## Examples:
+
+* Add an inbound rule with no encapsulation security for messenger.exe:
+  
+    ```netsh advfirewall firewall add rule name="allow messenger" dir=in program="c:\programfiles\messenger\msmsgs.exe" security=authnoencap action=allow```
+
+* Add an outbound rule for port 80:
+  
+    ```netsh advfirewall firewall add rule name="allow80" protocol=TCP dir=out localport=80 action=block```
+
+* Add an inbound rule requiring security and encryption for TCP port 80 traffic:
+  
+    ```netsh advfirewall firewall add rule name="Require Encryption for Inbound TCP/80" protocol=TCP dir=in localport=80 security=authdynenc action=allow```
+
+* Add an inbound rule for messenger.exe and require security
+  
+    ```netsh advfirewall firewall add rule name="allow messenger" dir=in program="c:\program files\messenger\msmsgs.exe" security=authenticate action=allow```
+
+* Add an authenticated firewall bypass rule for group acmedomain\scanners identified by a SDDL string:
+  
+    ```netsh advfirewall firewall add rule name="allow scanners" dir=in rmtcomputergrp=<SDDL string> action=bypass security=authenticate```
+
+* Add an outbound allow rule for local ports 5000-5010 for udp-
+* Add rule name="Allow port range" dir=out protocol=udp localport=5000-5010 action=allow
---
+++ src/index.ts
@@ -0,0 +1,181 @@
+import minimist from 'minimist';
+import chalk from 'chalk';
+import path from 'path';
+import fs from 'fs';
+import { exist, uniqueFileName, toUnix, toWindows } from './unique-names';
+
+type Args = {
+    name: string;       // name=<Name of the rule you want >
+    program: string;    // program=<”Path of the executable”>
+    enable: string;     // enable=yes or no
+    dir: string;        // dir=Inbound(in) or outbound(out) rule
+    action: string;     // action=allow or block or custom
+    profile: string;    // ( To add rule in more than one profile use “,” e.g.: profile=private, domain )
+    format: string;     // The output format: bat | ps1 | js (default is bat) // TODO:
+    //                  // Generated parameters
+    _: string[];        // Nameless parameters.
+    files: string [];   // collected after argumnets parsed.
+    dest: string;       // The destination output filename.
+    //unix: boolean;      // Convert back slashed to slashed.
+}
+
+function help(info?: string) {
+    const msg = 
+`
+netsh-rules will generate a batch file to manage inbound and outbound Windows
+firewall connections for a single exe file or all exe files in folder.
+
+Usage:
+    netsh-rules <absolute path to filename | absolute path to folder> [options]
+
+Options:
+    --name    - The rule name (name will be generated if missing name)
+    --program - Absolute path to <filename | folder>
+    --enable  - Enable rule: yes | no (default: yes)
+    --dir     - The rule is inbound or outbound: in | out | both (default: both i.e. in and out)
+    --action  - The action for rule: allow | block (default: block)
+    --profile - Apply rule for: public | private | domain (default: public, private, domain)
+`;
+    //TODO: format - the output format: bat | ps1 | js (default is bat)
+    //unix - convert slashes in pathes to Unix format (default true) - does not make sence anymore
+    console.log(chalk.gray(msg));
+
+    if (info) {
+        console.log(chalk.red(info));
+    }
+
+    console.log('');
+}
+
+function checkArgs() {
+    let args = minimist(process.argv.slice(2), {
+        string: ['name', 'action', 'enable', 'dir', 'profile', 'program'],
+        boolean: ['unix'],
+        default: {
+            name: '__Generated__', // TODO: Add last folder or filename from program
+            enable: 'yes',
+            action: 'block',
+            dir: 'both',
+            profile: 'public,private,domain',
+            //
+            files: [],
+            format: 'bat',
+            //unix: true,
+        }
+    }) as Args;
+
+    !args.program && (args.program = args._[0]);
+
+    //
+    //TODO: name
+    checkArg(args.enable, 'enable', ['yes', 'no']);
+    checkArg(args.action, 'action', ['allow', 'block']);
+    checkArg(args.dir, 'dir', ['in', 'out', 'both']);
+    checkArg(args.profile, 'profile', ['public', 'private', 'domain']);
+
+    //
+
+    if (!args.program) {
+        help('No program to process');
+        process.exit(1);
+    }
+
+    const st = exist(args.program);
+    if (!st) {
+        help(`Program not found: ${args.program}`);
+        process.exit(2);
+    }
+
+    if (st.isDirectory()) {
+        let files = [];
+        collectExeFiles(args.program, files, true);
+        args.files = filterDuplicated(files);
+    } else {
+        args.files.push(args.program);
+
+    }
+    //args.unix && (args.files = args.files.map(toUnix));
+    args.files = args.files.map(toWindows); // netsh accepts only back slashes
+
+    args.files.sort((a, b) => a.length - b.length); // shortest first
+    
+    console.log(chalk.yellow('Found exe files:'));
+    console.log(chalk.yellow(`${args.files.map((_) => `    ${_}\n`)}`));
+
+    args.dest = `${uniqueFileName('netsh-rules')}.txt`;
+
+    return args;
+
+    function collectExeFiles(folder: string, rv: string[], recursive: boolean): void {
+        rv.push(...fs.readdirSync(folder).map((_) => {
+            let fname = path.join(folder, _);
+            let _st = fs.statSync(fname);
+            if (_st.isDirectory()) {
+                recursive && collectExeFiles(fname, rv, recursive);
+            } else {
+                return _st.isFile() && path.extname(_).toLowerCase() === '.exe' ? fname : '';
+            }
+        }).filter(Boolean));
+    }
+
+    function filterDuplicated(names: string[]): string[] {
+        return [...new Map(names.map(_ => [_.toLowerCase(), _])).values()]; // leave unique and preserve names case.
+    }
+
+    function checkArg(value: string, name: string, allowed: string[]): void {
+        let s = (value || '').trim();
+        if (!s) {
+            help(`Required argument '${name}' is missing. Allowed values are '${allowed.join(' | ')}'`);
+            process.exit(3);
+        }
+        let arr = s.toLowerCase().split(',').map(_ => _.trim().toLowerCase());
+        arr.forEach((src) => {
+            if (!allowed.includes(src)) {
+                help(`Invalid argument '${name} = ${value}'. Allowed values are '${allowed.join(' | ')}'`);
+                process.exit(3);
+            }
+        });
+    }
+} //checkArgs()
+
+function ruleName(fname: string): string {
+    fname = path.normalize(fname);
+    let parentfolder = path.dirname(fname).split(path.sep).pop().replace(/ /g, '_');
+    return `__generated: ${parentfolder}: ${path.basename(fname)}__`;
+}
+
+function genearateBatchFile(args: Args) { //: string[] | undefined
+    // netsh advfirewall firewall show rule name="all"
+    // netsh advfirewall firewall delete rule "<Rule Name>"
+    // to verify run wf.msc
+    const NETSH = 'netsh advfirewall firewall add rule';
+
+    let base = [
+        `enable=${args.enable}`,
+        `action=${args.action}`,
+        `profile=${args.profile}`
+    ].join(' ');
+
+    let cmds: string[]  = [];
+    args.files.forEach((_) => {
+        let name = ruleName(_);
+        if (args.dir === 'both') {
+            cmds.push(`${NETSH} dir=in  name="${name}" ${base} program="${_}"`);
+            cmds.push(`${NETSH} dir=out name="${name}" ${base} program="${_}"`);
+        } else {
+            cmds.push(`${NETSH} dir=${args.dir} name="${name}" ${base} program="${_}"`);
+        }
+    });
+    //console.log(chalk.yellow(JSON.stringify(cmds, null, 4)));
+    cmds.forEach((_) => console.log(chalk.yellow(_)));
+}
+
+function main() {
+    let args = checkArgs();
+    //console.log(chalk.yellow(JSON.stringify(args, null, 4)));
+
+    genearateBatchFile(args);
+} //main()
+
+main();
+
